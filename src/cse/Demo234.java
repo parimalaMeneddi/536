db7adc1a56362d18b14a41fe7e4f65d5cd720c82
parimalaMeneddi <115864493+parimalaMeneddi@users.noreply.github.com>
2022-10-17 23:16:54 -0700

    print statement

--- src/cse/Demo234.java
+++ src/cse/Demo234.java
@@ -5,6 +5,7 @@ public class Demo234 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.print("hello);
+       System.out.print("welcometo cse");
 	}
 
 }
